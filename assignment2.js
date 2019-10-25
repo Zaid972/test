2ea7f7a8b3bb23aa2d23c71dbcd6aa6eb0789105
Zaid972 <55143136+Zaid972@users.noreply.github.com>
2019-10-25 11:03:14 -0400

    comment

--- assignment2.js
+++ assignment2.js
@@ -1,36 +1,30 @@
 
+// function o(){
+//   var x =0;
+//    while(x<=99){
+// 	x = x +1;
+// 	if(x % 4 == 0 && x % 10 == 0) {
+// 	console.log("fourteen");
+// 	}else if(x % 10 == 0){
+// 	console.log("ten");
+// 	}else if(x % 4 == 0){
+// 	console.log("four");
 
-
-
-
-
-
-function o(){
-  var x =0;
-   while(x<=99){
-	x = x +1;
-	if(x % 4 == 0 && x % 10 == 0) {
-	console.log("fourteen");
-	}else if(x % 10 == 0){
-	console.log("ten");
-	}else if(x % 4 == 0){
-	console.log("four");
-
-    }
-  }
+//     }
+//   }
 	
-}
+// }
 
 
 
 
 
 
-function giveyear(){
-	var year = prompt("give me a year and i might give you two centuries");
-	var century = year/100 = 1;
-	console.log (century)
-	prompt("that year is the " + Math.floor (century) + " century");
+// function giveyear(){
+// 	var year = prompt("give me a year and i might give you two centuries");
+// 	var century = year/100 = 1;
+// 	console.log (century)
+// 	prompt("that year is the " + Math.floor (century) + " century");
 
 
 
@@ -38,23 +32,23 @@ function giveyear(){
 
 
 
-}
+// }
 
 
- function triangle (){
- 	var y = prompt("Enter the other angle of that same triangle(Just the number)");
- 	var x = prompt("enter an angle belonging to a triangle)");
- 	alert(180 - x -y + "" + " degree is the 3rdangle of the triangle");
+//  function triangle (){
+//  	var y = prompt("Enter the other angle of that same triangle(Just the number)");
+//  	var x = prompt("enter an angle belonging to a triangle)");
+//  	alert(180 - x -y + "" + " degree is the 3rdangle of the triangle");
 
- 	 }
-
-
-    function test3(){
-    	alert("A");
-    	console.log("Hi");
+//  	 }
 
 
-    }
+//     function test3(){
+//     	alert("A");
+//     	console.log("Hi");
+
+
+//     }
 
 
      
