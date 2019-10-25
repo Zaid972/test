29939092aa70876299e08c36db3834f2497d181e
Zaid972 <55143136+Zaid972@users.noreply.github.com>
2019-10-25 11:01:33 -0400

    jslink testing

---
+++ CODE ASSIGNMENT 2.js
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>"Zaid's website"</title>
+    <script type="text/javascript"src="CODE ASSIGNMENT2.js"></script>
+    <link rel="stylesheet" type="text/css" href="main.css">
+    </head>
+    <hr><hr/>
+    <hr><hr/>
+
+
+
+    <body>
+    <h1>Zaid's website</h1>
+    <
+
+
+
+
+
+
+function o(){
+  var x =0;
+   while(x<=99){
+	x = x +1;
+	if(x % 4 == 0 && x % 10 == 0) {
+	console.log("fourteen");
+	}else if(x % 10 == 0){
+	console.log("ten");
+	}else if(x % 4 == 0){
+	console.log("four");
+
+    }
+  }
+	
+}
+
+
+
+
+
+
+function giveyear(){
+	var year = prompt("give me a year and i might give you two centuries");
+	var century = year/100 = 1;
+	console.log (century)
+	prompt("that year is the " + Math.floor (century) + " century");
+
+
+
+
+
+
+
+}
+
+
+ function triangle (){
+ 	var y = prompt("Enter the other angle of that same triangle(Just the number)");
+ 	var x = prompt("enter an angle belonging to a triangle)");
+ 	alert(180 - x -y + "" + " degree is the 3rdangle of the triangle");
+
+ 	 }
+
+
+    function test3(){
+    	alert("A");
+    	console.log("Hi");
+
+
+    }
+
+
+     
+     Function test4(){
+     	alert("Man");
+     	console.log("Hey");
+     }
+
+
+
+
---
+++ }.js
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>"Zaid's website"</title>
+    <script type="text/javascript"src="CODE ASSIGNMENT2.js"></script>
+    <link rel="stylesheet" type="text/css" href="main.css">
+    </head>
+    <hr><hr/>
+    <hr><hr/>
+
+
+
+    <body>
+    <center>
+    <h1>Timothy Spark's website</h1>
+    <h6>credits to cilantro</h6>
+    </center>
+
+
+
+
+
+function o(){
+  var x =0;
+   while(x<=99){
+	x = x +1;
+	if(x % 4 == 0 && x % 10 == 0) {
+	console.log("fourteen");
+	}else if(x % 10 == 0){
+	console.log("ten");
+	}else if(x % 4 == 0){
+	console.log("four");
+
+    }
+  }
+	
+}
+
+
+
+
+
+
+function giveyear(){
+	var year = prompt("give me a year and i might give you two centuries");
+	var century = year/100 = 1;
+	console.log (century)
+	prompt("that year is the " + Math.floor (century) + " century");
+
+
+
+
+
+
+
+}
+
+
+ function triangle (){
+ 	var y = prompt("Enter the other angle of that same triangle(Just the number)");
+ 	var x = prompt("enter an angle belonging to a triangle)");
+ 	alert(180 - x -y + "" + " degree is the 3rdangle of the triangle");
+
+ 	 }
+
+
+    function test3(){
+    	alert("A");
+    	console.log("Hi");
+
+
+    }
+
+
+     
+     Function test4(){
+     	alert("Man");
+     	console.log("Hey");
+     }
+
+
+
+
