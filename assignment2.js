d2ee54f1b8978cca6dcccff6c94f836060c483fc
Zaid972 <55143136+Zaid972@users.noreply.github.com>
2019-11-05 11:13:30 -0500

    Text color

--- assignment2.js
+++ assignment2.js
@@ -1,30 +1,30 @@
 
-// function o(){
-//   var x =0;
-//    while(x<=99){
-// 	x = x +1;
-// 	if(x % 4 == 0 && x % 10 == 0) {
-// 	console.log("fourteen");
-// 	}else if(x % 10 == 0){
-// 	console.log("ten");
-// 	}else if(x % 4 == 0){
-// 	console.log("four");
+function o(){
+  var x =0;
+   while(x<=99){
+	x = x +1;
+	if(x % 4 == 0 && x % 10 == 0) {
+	console.log("fourteen");
+	}else if(x % 10 == 0){
+	console.log("ten");
+	}else if(x % 4 == 0){
+	console.log("four");
 
-//     }
-//   }
+    }
+  }
 	
-// }
+}
 
 
 
 
 
 
-// function giveyear(){
-// 	var year = prompt("give me a year and i might give you two centuries");
-// 	var century = year/100 = 1;
-// 	console.log (century)
-// 	prompt("that year is the " + Math.floor (century) + " century");
+function giveyear(){
+	var year = prompt("give me a year and i might give you two centuries");
+	var century = year/100+ 1;
+	console.log (century)
+	prompt("that year is the " + Math.floor (century) + " century");
 
 
 
@@ -32,30 +32,45 @@
 
 
 
-// }
+}
 
 
-//  function triangle (){
-//  	var y = prompt("Enter the other angle of that same triangle(Just the number)");
-//  	var x = prompt("enter an angle belonging to a triangle)");
-//  	alert(180 - x -y + "" + " degree is the 3rdangle of the triangle");
+ function triangle (){
+ 	var y = prompt("Enter the other angle of that same triangle(Just the number)");
+ 	var x = prompt("enter an angle belonging to a triangle)");
+ 	alert(180 - x -y + "" + " degree is the 3rdangle of the triangle");
 
-//  	 }
-
-
-//     function test3(){
-//     	alert("A");
-//     	console.log("Hi");
+ 	 }
 
 
-//     }
+    function test3(){
+    	alert("A");
+    	console.log("Hi");
+
+
+    }
 
 
      
-     function test4(){
-     	alert("Man");
-     	console.log("Hey");
-     }
+     function Tenfour(){
+         for(let i=0 ; i<101 ; i+=1){ 
+             if(i%4==0)
+             console.log ("Four");
+
+            else if (i%10==0)
+            	console.log ("Ten");
+            else if (i%4==0 && i%10==0)
+            	console.log("TenFour");
+            else{    
+
+
+            }
+
+
+            }
+
+         }
+  
 
 
 
